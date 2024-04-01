e461a0ba0345430f251ffa7d0930a41c43503825
sdklick <sumantad827@gmail.com>
2024-04-01 13:11:19 +0530

    Change home page ui

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,7 @@ import Footer from "./Footer";
 
 const Home = () => {
   const [load1, set1] = useState(false);
-  
+
   return (
     <>
       <Nav />
@@ -34,56 +34,59 @@ const Home = () => {
           ></button>
         </div>
         <div className="carousel-inner">
-        {load1?
-          <div className="carousel-item active">
-          
-            <img
-              
-              src="https://source.unsplash.com/1600x700/?technology"
-              className="d-block w-100"
-              alt="..."
-            />
-            <div
-              className="carousel-caption  d-block position-absolute top-50 start-50 translate-middle border rounded-pill border-info "
-              style={{ backgroundColor: "rgba(0,0,0, 0.7)" }}
-            >
-              <i
+          {load1 ? (
+            <div className="carousel-item active">
+              <img
+                src="https://source.unsplash.com/1920x1080/?technology"
+                className="d-block w-100"
+                style={{ height: "300px" }}
+                alt="..."
+              />
+              <div
+                className="carousel-caption  d-block position-absolute top-50 start-50 translate-middle border rounded-pill border-info "
+                style={{ backgroundColor: "rgba(0,0,0, 0.7)" }}
+              >
+                {/* <i
                 className="fas fa-laptop-code"
                 style={{ fontSize: "50px", color: "greenyellow" }}
               ></i>
-              <h3 className="mt-2">Welcome To Online Pro Exam </h3>
-              <h5 className="mt-2">Set Your Exam</h5>
-              <NavLink to="/setquestion">
-                <button
-                  type="button"
-                  className="btn btn-primary border rounded-pill btn-sm mt-3 mb-3"
-                >
-                  <i
-                    className="fas fa-angle-double-right"
-                    style={{ fontSize: "30px", color: "greenyellow" }}
-                  ></i>
-                </button>
-              </NavLink>
-              
+              <h3 className="mt-2">Welcome To Online Pro Exam </h3> */}
+                <h5 className="mt-2">Check Your Result</h5>
+
+                <NavLink to="/examresult">
+                  <button
+                    type="button"
+                    className="btn btn-primary border rounded-pill btn-sm mt-3 mb-3"
+                  >
+                    <i
+                      className="fas fa-angle-double-right"
+                      style={{ fontSize: "30px", color: "greenyellow" }}
+                    ></i>
+                  </button>
+                </NavLink>
+              </div>
             </div>
-          </div>:<Skeleton count={1000}/>}
+          ) : (
+            <Skeleton count={1000} />
+          )}
           <div className="carousel-item">
             <img
-             onLoad={() => set1(true)}
-              src="https://source.unsplash.com/1600x700/?programming"
+              onLoad={() => set1(true)}
+              src="https://source.unsplash.com/1920x1080/?programming"
               className="d-block w-100"
+              style={{ height: "300px" }}
               alt="..."
             />
             <div
               className="carousel-caption  d-block d-block position-absolute top-50 start-50 translate-middle border rounded-pill border-info"
               style={{ backgroundColor: "rgba(0,0,0, 0.7)" }}
             >
-              <i
+              {/* <i
                 className="fas fa-award"
                 style={{ fontSize: "50px", color: "greenyellow" }}
               ></i>
-              <h3 className="mt-2">Welcome To Online Pro Exam </h3>
-              
+              <h3 className="mt-2">Welcome To Online Pro Exam </h3> */}
+
               <h5 className="mt-2">Give Your Exam</h5>
               <NavLink to="/exampage">
                 <button
@@ -96,7 +99,6 @@ const Home = () => {
                   ></i>
                 </button>
               </NavLink>
-              
             </div>
           </div>
         </div>
@@ -106,7 +108,10 @@ const Home = () => {
           data-bs-target="#carouselExampleCaptions"
           data-bs-slide="prev"
         >
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
           <span className="visually-hidden">Previous</span>
         </button>
         <button
@@ -115,13 +120,13 @@ const Home = () => {
           data-bs-target="#carouselExampleCaptions"
           data-bs-slide="next"
         >
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
           <span className="visually-hidden">Next</span>
         </button>
-       
       </div>
-      
-      
     </>
   );
 };
