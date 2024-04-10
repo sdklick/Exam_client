dc0e0eeb1312607a2183720f5ecdb016df5f980f
sdklick <sumantad827@gmail.com>
2024-04-10 13:22:47 +0530

    useEffect add

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState , useEffect } from "react";
 import Nav from "./Nav";
 import { NavLink } from "react-router-dom";
 import Skeleton from "react-loading-skeleton";
